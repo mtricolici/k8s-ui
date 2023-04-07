e16f0df64948e023cf5317a6211a3cf3b2a7924b
Marcel Tricolici <marcel@tricolici.com>
2023-04-07 04:48:35 +0300

    replacing kubectl with k8s api golang

---
+++ k8s/query/namespaces.go
@@ -0,0 +1,39 @@
+package query
+
+import (
+	"context"
+	"k8s_ui/utils"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func Namespaces(client *kubernetes.Clientset) ([][]string, error) {
+	ns_list, err := client.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	namespaces := [][]string{{"NAME", "STATUS", "AGE"}}
+
+	for _, ns := range ns_list.Items {
+		row := []string{
+			ns.Name,
+			namespace_status(ns),
+			namespace_age(ns),
+		}
+		namespaces = append(namespaces, row)
+	}
+
+	return namespaces, nil
+}
+
+func namespace_status(ns v1.Namespace) string {
+	return string(ns.Status.Phase)
+}
+
+func namespace_age(ns v1.Namespace) string {
+	return utils.HumanElapsedTime(ns.CreationTimestamp.Time)
+}
--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+	"time"
+)
 
 func ShortString(str string, max int) string {
 	slen := len(str)
@@ -14,4 +18,30 @@ func ShortString(str string, max int) string {
 func ReplaceSpecialChars(str string) string {
 	regex := regexp.MustCompile("[^a-zA-Z0-9_-]+")
 	return regex.ReplaceAllString(str, "")
+}
+
+func HumanElapsedTime(t time.Time) string {
+	now := time.Now()
+	elapsed := now.Sub(t)
+
+	if elapsed < 5*time.Minute {
+		return fmt.Sprintf("%ds", int(elapsed.Seconds()))
+	} else if elapsed < 5*time.Hour {
+		return fmt.Sprintf("%dm%02ds", int(elapsed.Minutes()), int(elapsed.Seconds())%60)
+	} else if elapsed < 48*time.Hour {
+		return fmt.Sprintf("%dh%02dm", int(elapsed.Hours()), int(elapsed.Minutes())%60)
+	}
+
+	days := int(elapsed.Hours() / 24)
+	if days <= 60 {
+		return fmt.Sprintf("%dd", days)
+	}
+
+	months := days / 30
+	if months <= 12 {
+		return fmt.Sprintf("%dmo", months)
+	}
+
+	years := months / 12
+	return fmt.Sprintf("%dy", years)
 }

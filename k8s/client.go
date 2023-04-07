288230e947020685a1377d98acee5ba513ef36a3
Marcel Tricolici <marcel@tricolici.com>
2023-04-07 06:31:45 +0300

    implement get deployments

--- k8s/client.go
+++ k8s/client.go
@@ -19,6 +19,14 @@ func GetResources(ns, resource string, wide bool) ([][]string, error) {
 		return query.Pods(ns, wide)
 	case "service":
 		return query.Services(ns, wide)
+	case "deployment":
+		return query.Deployments(ns, wide)
+	case "ingress":
+	case "pvc":
+	case "daemonset":
+	case "replicaset":
+	case "statefulset":
+	case "all":
 	}
 
 	return nil, fmt.Errorf("get resource '%s' not implemented yet", resource)
